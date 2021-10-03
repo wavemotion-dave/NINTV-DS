50efdd4279cc2dfad7b1b9837834508c6b87e7f3
wavemotion-dave <75039837+wavemotion-dave@users.noreply.github.com>
2021-10-03 09:16:48 -0400

    Cleanup and headers added.

--- arm9/source/emucore/ripxbf.h
+++ arm9/source/emucore/ripxbf.h
@@ -12,14 +12,6 @@
 extern "C" {
 #endif
 
-//compatibility flags
-typedef enum _BiosCompatibility
-{
-    BIOS_PREFERRED = 0,
-    BIOS_COMPATIBLE = 1,
-    BIOS_INCOMPATIBLE = 2
-} BiosCompatibility;
-
 typedef enum _PeripheralCompatibility
 {
     PERIPH_REQUIRED = 0,
@@ -27,16 +19,6 @@ typedef enum _PeripheralCompatibility
     PERIPH_COMPATIBLE = 2,
     PERIPH_INCOMPATIBLE = 3
 } PeripheralCompatibility;
-
-UINT64 freadInt(FILE*, UINT32 length);
-UINT16 freadUINT16(FILE*);
-UINT32 freadUINT32(FILE*);
-UINT64 freadUINT64(FILE*);
-void freadString(FILE*, CHAR* s, UINT32 maxLength);
-void fwriteUINT16(FILE*, UINT16);
-void fwriteUINT32(FILE*, UINT32);
-void fwriteUINT64(FILE*, UINT64);
-void fwriteString(FILE*, const CHAR*);
 
 //the RIP magic number
 #define RIP_MAGIC_NUMBER 0x3F383A347651B5DAll
@@ -61,58 +43,6 @@ void fwriteString(FILE*, const CHAR*);
 #define ID_SYSTEM_INTELLIVISION     0x4AC771F8
 #define ID_PERIPH_ECS               0x143D9A97
 #define ID_PERIPH_INTELLIVOICE      0x6EFF540A
-
-//bios numbers
-#define A5200_P_MASTER_COMPONENT    0
-#define A5200_BT_BIOS               0
-#define A5200_B_ORIGINAL_BIOS       0
-
-#define INTV_P_MASTER_COMPONENT     0
-#define INTV_BT_EXEC                0
-#define INTV_B_ORIGINAL_EXEC        0
-#define INTV_B_INTY2_EXEC           1
-#define INTV_B_SEARS_EXEC           2
-#define INTV_B_GPL_EXEC             3
-#define INTV_BT_GROM                1
-#define INTV_B_ORIGINAL_GROM        0
-#define INTV_B_GPL_GROM             1
-#define INTV_P_ECS                  1
-#define INTV_BT_ECS_BIOS            0
-#define INTV_B_ORIGINAL_ECS_BIOS    0
-#define INTV_P_INTELLIVOICE         2
-#define INTV_BT_IVOICE_BIOS         0
-#define INTV_B_ORIGINAL_IVOICE_BIOS 0
-
-/*
-typedef struct _RipBios
-{
-    CHAR*       biosName;
-    UINT32      biosCRC32;
-} RipBios;
-
-typedef struct _RipBiosType
-{
-    CHAR*       biosTypeName;
-    RipBios        bioses[16];
-} RipBiosType;
-
-typedef struct _RipPeripheral
-{
-    CHAR*       peripheralName;
-    RipBiosType    biosTypes[16];
-} RipPeripheral;
-
-typedef struct _RipSystem
-{
-    UINT32      systemID;
-    CHAR*       systemName;
-    UINT8       addressByteWidth;
-    UINT8       valueByteWidth;
-    RipPeripheral  peripherals[16];
-} RipSystem;
-
-RipSystem* getSystem(UINT32 systemID);
-*/
 
 #ifdef __cplusplus
 }
